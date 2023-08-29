2409797e921e608637ea8fdbeedac0b1f8f3cd61
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-08-29 08:42:02 -0300

    Add files via upload

---
+++ trocavariaveis.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+ int main()
+{
+    int A, B, C;
+
+    printf("Escreva o valor A: ");
+    printf("Escreva o valor B: ");
+    printf("Escreva o valor C: ");
+    scanf("%i",&A);
+    scanf("%i",&B);
+    scanf("%i",&C);
+
+}
