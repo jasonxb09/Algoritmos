57dd042515018b43835e80869897d46048bfd2cb
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-09-05 08:41:15 -0300

    Add files via upload

---
+++ soma_dos_quadrados.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c, d, e, f, g;
+    printf("escreva a:  ");
+    scanf("%i", &a);
+    printf("escreva b:  ");
+    scanf("%i", &b);
+    printf("escreva c:  ");
+    scanf("%i", &c);
+    d=a*a;
+    e=b*b;
+    f=c*c;
+    g=d+e+f;
+    printf("%i", g);
+}
