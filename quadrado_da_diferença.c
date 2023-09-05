2ebea67d945b890597777a70736b5ce7bf33fe45
jasonxb09 <142895801+jasonxb09@users.noreply.github.com>
2023-09-05 08:28:51 -0300

    Add files via upload

---
+++ quadrado_da_diferença.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a, b, c, d ;
+    printf("Escreva a:  ");
+    scanf("%i", &a);
+    printf("Escreva b:  ");
+    scanf("%i", &b);
+    c=a-b;
+    d=c*c;
+    printf("%i", d);
+}
